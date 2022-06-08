c4ac6ec894fb69244a485165484112a6262d692a
Erich Elste <erichelste@yahoo.com>
2022-06-08 14:43:46 -0400

    Login page created, pages folder made

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,68 @@
-import loginForm from "../components/loginForm";
+// import LoginForm from "./components/LoginForm";
 import React, { useState } from "react";
+import Axios from 'axios'
 
 function Login() {
-    const devUser = {
-        email: "test@test.com",
-        password: "test123"
-    }
 
-    const [user, setUser] = useState({ name: "", email: "" });
-    const [error, setError] = useState("");
+    const [usernameReg, setUsernameReg] = useState('')
+    const [passwordReg, setPasswordReg] = useState('')
 
-    const Login = details => {
-        console.log(details);
-    }
+    const create = () => {
+        Axios.post('http://localhost3000/create', {username: usernameReg, password: passwordReg})
+        .then((response) => {
+            console.log(response);
+        });
+    };
 
-    const Logout = () => {
-        console.log("logout");
-    }
     return (
         <div className="App">
-
-
+            <div className="createAccount">
+                <h1>Create Account</h1>
+                <label>Username</label>
+                <input
+                type="text"
+                onChange={(e) =>{
+                    setUsernameReg(e.target.value);
+                }}
+                />
+                <label>Password</label>
+                <input type="text" onChange={(e) =>{
+                    setPasswordReg(e.target.value);
+                }}
+                />
+                <button onClick={create}>Create!</button>
+            </div>
+            <div className="login">
+                <h1>Login</h1>
+                <input type="text" placeholder="Username..."/>
+                <input type="password" placeholeder="Password..."/>
+                <button>Login!</button>
+            </div>
         </div>
     );
-}
+
+
+    // const devUser = {
+    //     email: "test@test.com",
+    //     password: "test123"
+    // }
+
+    // const [user, setUser] = useState({ name: "", email: "" });
+    // const [error, setError] = useState("");
+
+    // const Login = details => {
+    //     console.log(details);
+    // }
+
+    // const Logout = () => {
+    //     console.log("logout");
+    // }
+    // return (
+    //     <div className="App">
+
+
+    //     </div>
+    // );
+};
 
 export default Login;
