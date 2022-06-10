98e8a56e9c174d65772079d3941f6264f8a76f18
AJLosey <aaronlosey11@gmail.com>
2022-06-10 10:51:10 -0500

    sign up page styling

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import ViewComments from './pages/ViewComments';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ViewComments />
+    <Login />
   </React.StrictMode>
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,35 +8,48 @@ function Login() {
     const [passwordReg, setPasswordReg] = useState('')
 
     const create = () => {
-        Axios.post('http://localhost3000/create', {username: usernameReg, password: passwordReg})
-        .then((response) => {
-            console.log(response);
-        });
+        Axios.post('http://localhost3000/create', { username: usernameReg, password: passwordReg })
+            .then((response) => {
+                console.log(response);
+            });
     };
 
     return (
-        <div className="App">
-            <div className="createAccount">
-                <h1>Create Account</h1>
-                <label>Username</label>
-                <input
-                type="text"
-                onChange={(e) =>{
-                    setUsernameReg(e.target.value);
-                }}
-                />
-                <label>Password</label>
-                <input type="text" onChange={(e) =>{
-                    setPasswordReg(e.target.value);
-                }}
-                />
-                <button onClick={create}>Create!</button>
+        <div className="App bg-gradient-to-tl from-black to-gray-500 h-screen w-screen text-white">
+            <div className="createAccount ">
+                <h1 className="h-[50px] w-screen text-transparent bg-clip-text bg-gradient-to-br from-[#ff8300] to-[#00d8ff] border-b-4 border-b-indigo-500 flex justify-center items-center text-4xl font-extrabold">Create Account</h1>
+
+                <div className='m-4'>
+                    <div className='mb-4'>
+                        <label className='m-8'>Username</label>
+                        <input
+                            type="text"
+                            onChange={(e) => {
+                                setUsernameReg(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <label className='m-8'>Password</label>
+                        <input type="text" onChange={(e) => {
+                            setPasswordReg(e.target.value);
+                        }}
+                        />
+                    </div>
+                </div>
+
+                <button className='mb-4 rounded bg-gray-600 font-bold text-md md:text-2xl text-slate-300 md:py-2 p-[5px] md:flex-1' onClick={create}>Create!</button>
+
             </div>
+
             <div className="login">
-                <h1>Login</h1>
-                <input type="text" placeholder="Username..."/>
-                <input type="password" placeholeder="Password..."/>
-                <button>Login!</button>
+                <div>
+                    <input className='m-4' type="text" placeholder="Username..." />
+                </div>
+                <div>
+                    <input className='m-4' type="password" placeholder="Password..." />
+                </div>
+                <button className='mb-4 rounded bg-gray-600 font-bold text-md md:text-2xl text-slate-300 md:py-2 p-[5px] md:flex-1'>Login!</button>
             </div>
         </div>
     );
